66bdd34b23d3d6cc48ec7e3b21f9e69ae02ff000
silesky <seth.silesky@gmail.com>
2016-10-17 16:31:46 -0500

    update read methods

--- app.js
+++ app.js
@@ -2,24 +2,51 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 //http://mongodb.github.io/node-mongodb-native/2.2/quick-start/
 //https://docs.mongodb.com/v3.2/reference/method/db.collection.find/
-let friends
+let all, friends, gifts;
 MongoClient.connect('mongodb://127.0.0.1:27017/giftr', (err, db) => {
     if (err) throw err;
     console.log('...connected to mongoDB!');
     const myCollection = db.collection('friendsCollection'); 
     myCollection.find().toArray((err, docs) => {
-         friends = docs;
-    });	
+        all = docs;
+        friends = docs.map(el => el.name);
+        gifts = docs.map(el => el.gifts);
+    }); 
 });
 
-/*
-// gifts
-{
-  "_id": "58051fc76ed64846830b4098",
-  "item": "Baseball"
-}
 
-*/
 const app = express();
+app.get('/api/', (req, res) => res.json(all));
 app.get('/api/friends', (req, res) => res.json(friends));
-app.listen(3000);		
+/*
+[
+  "Nick",
+  "Dan",
+  "Stephen"
+]
+*/
+app.get('/api/gifts', (req, res) => res.json(gifts));
+
+/* 
+[
+  [
+    {
+      "giftType": "Book",
+      "giftName": "The Once and Future King"
+    }
+  ],
+  [
+    {
+      "giftType": "Record",
+      "giftName": "The Beatles Revolver"
+    }
+  ],
+  [
+    {
+      "giftType": "Gadget",
+      "giftName": "Serfas Bike Light"
+    }
+  ]
+]
+*/
+app.listen(3000);       
