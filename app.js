943940775b46d0255f8d53417a3491141a1db30d
silesky <seth.silesky@gmail.com>
2016-10-20 16:05:17 -0500

    add giftCollection

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json())
 
-let all, friends, gifts, myCollection;
+let all, friends, gifts, friendCollection, giftCollection;
 MongoClient.connect('mongodb://127.0.0.1:27017/giftr', (err, db) => {
     if (err) throw err;
     console.log('...connected to mongoDB!');
-    myCollection = db.collection('friendsCollection'); 
-    myCollection.find().toArray((err, docs) => {
+    friendCollection = db.collection('friendCollection'); 
+    giftCollection = db.collection('giftCollection'); 
+    friendCollection.find().toArray((err, docs) => {
         all = docs;
         friends = docs.map(el => el.name);
         gifts = docs.map(el => el.gifts);
@@ -22,48 +23,30 @@ MongoClient.connect('mongodb://127.0.0.1:27017/giftr', (err, db) => {
 
 app.get('/api/', (req, res) => res.json(all));
 app.get('/api/friends', (req, res) => res.json(friends));
-/*
-[
-  "Nick",
-  "Dan",
-  "Stephen"
-]
-*/
 app.get('/api/gifts', (req, res) => res.json(gifts));
-
-/* 
-[
-  [
-    {
-      "giftType": "Book",
-      "giftName": "The Once and Future King"
-    }
-  ],
-  [
-    {
-      "giftType": "Record",
-      "giftName": "The Beatles Revolver"
-    }
-  ],
-  [
-    {
-      "giftType": "Gadget",
-      "giftName": "Serfas Bike Light"
-    }
-  ]
-]
-*/
-
-  //send a newUser as a json object
+  //send a newUser as a json object { "name": "bilssl"}
   app.post('/api/friends', (req, res) => {
       if (!req.body) return res.sendStatus(400)
       try {
-        myCollection.insert(req.body);
+        friendCollection.insert(req.body);
       } catch(e) {
         res.json({success: false, error: e })
       }
       res.json({success: true})
      
   });
+
+  app.post('/api/gifts', (req, res) => {
+     if (!req.body) return res.sendStatus(400)
+      try {
+        giftCollection.insert(req.body);
+      } catch(e) {
+        res.json({success: false, error: e })
+      }
+      res.json({success: true})
+     
+  }
+  )
+
 
 app.listen(3000);       
