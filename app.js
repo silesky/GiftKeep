cd777fcace22c9540cf31297650eb1fb7f9ccf51
Seth Silesky <seth.silesky@gmail.com>
2016-10-27 14:56:45 -0500

    Get idToken from query string

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.json())
+const cookieParser = require('cookie-parser');
 const fetch = require('node-fetch');
 
+const app = express();
+app.use(bodyParser.json())
+app.use(cookieParser());
+app.use(express.static('./public'));
 
-let all, friends, gifts, friendCollection, giftCollection;
+let friendCollection;
+let giftCollection;
 MongoClient.connect('mongodb://127.0.0.1:27017/giftr', (err, db) => {
     if (err) throw err;
     console.log('...connected to mongoDB!');
     friendCollection = db.collection('friendCollection');
     giftCollection = db.collection('giftCollection');
-    friendCollection.find().toArray((err, docs) => {
-        all = docs;
-        //friends = docs.map(el => el.friendName);
-        //gifts = docs.map(el => el.gifts);
-    });
 });
 
-app.use(express.static('./public'));
-app.get('/api/', (req, res) => res.json(all));
-app.get('/api/friends', (req, res) => res.json(friends));
-app.get('/api/gifts', (req, res) => res.json(gifts));
-  //send a newUser as a json object { "name": "bilssl"}
+app.get('/api/', (req, res) => {
+  friendCollection.find().toArray((err, docs) => res.json(docs));
+});
+app.get('/api/:token', (req, res) => { // takes a token, returns the user object
+  let token = req.params.token;
+  console.log('idToken from url:', token);
+  friendCollection.find().toArray((err, docs) => {
+    let result = docs.find(el => {
+      if (token === el.googleIdToken) {
+        return el
+      } else {
+        res.json({success: false, error: 'token not found'})
+      }
+    });
+    if (result) res.json(result);
+  });
+});
+
+//send a newUser as a json object { "name": "bilssl"}
 app.post('/api/friends', (req, res) => {
   if (!req.body) return res.sendStatus(400)
   try {
@@ -58,14 +71,9 @@ app.post('/oauthcallback', (req, res) => {
       //'Content-Type', 'application/x-www-form-urlencoded'
       if (el.status < 300) {
        console.log('token validated! statusText:', el.statusText);
-       console.log(el);
      } else {
         console.log('token invalid! statusText:', el.statusText);
       }})
   })
-
-
-
-
 app.listen(3000);
 console.log("listening on http://gifter.sethsilesky.com:3000");
--- public/index.js
+++ public/index.js
@@ -1,27 +1,29 @@
+console.log('index.js loaded.');
 const fetchPost = (route, data) => {
   console.log(typeof data);
-    return fetch(route, {
-      method: 'POST',
-      headers: new Headers({
-        'Content-Type': 'application/json'
-      }),
-      body: JSON.stringify({data: data}),
-    })
+  return fetch(route, {
+    method: 'POST',
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    }),
+    body: JSON.stringify({data: data}),
+  })
 }
 const server_url = "http://gifter.sethsilesky.com:3000/oauthcallback";
+
 
 function onSignIn(googleUser) {
   let profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
-console.log('Email: ' + profile.getEmail());
+  console.log('Email: ' + profile.getEmail());
   let idToken = googleUser.getAuthResponse().id_token;
   fetchPost(server_url, idToken).then(res => console.log(res.statusText));
 }
-  function signOut() {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-  }
+function signOut() {
+  const auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+  });
+}
