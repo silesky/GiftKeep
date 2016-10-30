8fa576af62f7f52f29e97f3c5b3a3bc8c15be20f
Seth Silesky <seth.silesky@gmail.com>
2016-10-30 14:26:12 -0500

    animation choppiness in drawer seems to be resolved

--- mobile/index.ios.js
+++ mobile/index.ios.js
@@ -29,80 +29,6 @@ import { lipsum } from './util';
 import Drawer from 'react-native-drawer';
 
 
-
-
-
-const ControlPanelStyles = StyleSheet.create({
-
-    })
-    // const DrawerStyles = StyleSheet.create({
-    //   container: {
-    //     flex: 1,
-    //     padding: 20,
-    //     backgroundColor: 'black',
-    //   },
-    //   controlText: {
-    //     color: 'white',
-    //   },
-    //   button: {
-    //     backgroundColor: 'white',
-    //     borderWidth: 1,
-    //     borderColor: 'black',
-    //     padding: 10,
-    //   }
-    // })
-
-// export class ControlPanel extends Component {
-
-//   render() {
-//     let {closeDrawer} = this.props
-//     return (
-//       <ScrollView style={ControlPanelStyles.container}>
-//         <Text style={ControlPanelStyles.controlText}>Control Panel</Text>
-//         <TouchableOpacity style={ControlPanelStyles.button} onPress={closeDrawer}>
-//           <Text>Close Drawer</Text>
-//         </TouchableOpacity>
-//       </ScrollView>
-//     )
-//   }
-// }
-
-// const ControlPanelStyles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     padding: 20,
-//     backgroundColor: 'black',
-//   },
-//   controlText: {
-//     color: 'white',
-//   },
-//   button: {
-//     backgroundColor: 'yellow',
-//     borderWidth: 1,
-//     borderColor: 'red',
-//     padding: 10,
-//   }
-// })
-
-// class Application extends Component {  
-//   closeControlPanel = () => {
-//     this._drawer.close()
-//   };
-//   openControlPanel = () => {
-//     this._drawer.open()
-//   };
-//   render () {
-//     return (
-//       <Drawer
-//         ref={(ref) => this._drawer = ref}
-//         content={<ControlPanel />}
-//         >
-
-//       </Drawer>
-//     )
-//   }
-// }
-
 export class CardExample extends Component {
     render() {
         return (
@@ -122,56 +48,45 @@ export class CardExample extends Component {
     }
 }
 
-export class ControlPanel extends Component {
-
-    render() {
-      return (
-        <Container>
-             <Header>
-                    <Button onPress={() => this.props.closeDrawer()} transparent>
-                          <Icon name='ios-menu' />
-                    </Button>
-                    <Title>Header</Title>
-                </Header>
- 
-        </Container>
-        );
-    }
-}
-
 class AppContainer extends Component {
-
     render() {
         return (
             <Drawer
+                tapToClose={true}
+                openDrawerOffset={0.2}
+                panCloseMask={0.2}
+                closedDrawerOffset={-3}
                 ref={(ref) => this._drawer = ref}
-                content={<ControlPanel closeDrawer={() => this._drawer.close()}
-                type="static"
-              />
-            }>
-              <Container> 
-                <Header>
-                    <Button onPress={() => this._drawer.open()} transparent>
-                        <Icon name='ios-menu' />
-                    </Button>
-                    <Title>Header</Title>
-                </Header>
-                <Content>
-                 
-                    <CardExample />
-                    <CardExample />
-                    <CardExample />
-                </Content>
-                <Footer>
-                    <FooterTab>
-                        <Button transparent>
-                            <Icon name='ios-call' />
-                              <Text>Footer HERE</Text>
-                        </Button>  
-                    </FooterTab>
-                </Footer>
-            </Container>
-          </Drawer>);
+                type='static'
+                content={  
+                    <View>
+                        <Text>{lipsum}</Text>
+                     </View>}
+                >
+                <Container> 
+                    <Header>
+                        <Button 
+                        onPress={() => this._drawer.open()} 
+                        transparent>
+                            <Icon name='ios-menu' />
+                        </Button>
+                        <Title>Header</Title>
+                    </Header>
+                    <Content>
+                        <CardExample />
+                        <CardExample />
+                        <CardExample />
+                    </Content>
+                    <Footer>
+                        <FooterTab>
+                            <Button transparent>
+                                <Icon name='ios-call' />
+                                  <Text>Footer HERE</Text>
+                            </Button>  
+                        </FooterTab>
+                    </Footer>
+                </Container>
+            </Drawer>);
     }
 }
 AppRegistry.registerComponent('mobile', () => AppContainer);
