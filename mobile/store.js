bd23565d2d2088a1171e06e80434fbda2c370a84
silesky <seth.silesky@gmail.com>
2016-10-31 16:12:30 -0500

    add redux thunk

--- mobile/store.js
+++ mobile/store.js
@@ -1,7 +1,9 @@
-//import devToolsEnhancer from 'remote-redux-devtools';
 import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk';
 import { reducer } from './reducer';
 
-export const store = createStore(reducer);
+export const store = createStore(reducer, applyMiddleware(thunk));
+
+
 
 
