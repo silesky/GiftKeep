73492d222c99e155ee6a5d3ee43a764a997c5582
Seth Silesky <seth.silesky@gmail.com>
2016-11-09 22:38:47 -0600

    facebook login install works

---
+++ mobile/app/components/FbLogin.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container } from 'native-base';
+const FBSDK = require('react-native-fbsdk');
+const {
+  LoginButton,
+} = FBSDK;
+      
+export const FbLogin = React.createClass({
+  render: function() {
+    return (
+        <LoginButton
+          publishPermissions={["publish_actions"]}
+          onLoginFinished={
+            (error, result) => {
+              if (error) {
+                alert("Login failed with error: " + result.error);
+              } else if (result.isCancelled) {
+                alert("Login was cancelled");
+              } else {
+                alert("Login was successful with permissions: " + result.grantedPermissions)
+              }
+            }
+          }
+          onLogoutFinished={() => alert("User logged out")}/>
+    );
+  }
+});
