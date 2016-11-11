3055dd267e0175b34b15e6b63eeae9b3ba9f5e0d
Seth Silesky <seth.silesky@gmail.com>
2016-11-11 00:09:20 -0600

    move to drawer

--- mobile/app/components/FbLogin.js
+++ mobile/app/components/FbLogin.js
@@ -1,27 +1,34 @@
-import React from 'react';
-import { Container } from 'native-base';
-const FBSDK = require('react-native-fbsdk');
-const {
-  LoginButton,
-} = FBSDK;
-      
-export const FbLogin = React.createClass({
-  render: function() {
+import React, { Component } from 'react';
+import { 
+  View,
+} from 'react-native';
+import {
+  Button
+} from 'native-base'
+
+import { LoginButton } from 'react-native-fbsdk';
+
+export class FbLogin extends Component {
+  render() {
     return (
-        <LoginButton
-          readPermissions={["public_profile", "user_friends"]}
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                alert("Login failed with error: " + result.error);
-              } else if (result.isCancelled) {
-                alert("Login was cancelled");
-              } else {
-                alert("Login was successful with permissions: " + result.grantedPermissions)
-              }
-            }
-          }
-          onLogoutFinished={() => alert("User logged out")}/>
+          <View style={{justifyContent: 'center'}}>
+            <Button transparent>
+              <LoginButton
+                readPermissions={["public_profile", "user_friends"]}
+                onLoginFinished={
+                  (error, result) => {
+                    if (error) {
+                      alert("Login failed with error: " + result.error);
+                    } else if (result.isCancelled) {
+                      alert("Login was cancelled");
+                    } else {
+                      alert("Login was successful with permissions: " + result.grantedPermissions)
+                    }
+                  }
+                }
+                onLogoutFinished={() => alert("User logged out")}/>
+            </Button>
+          </View>
     );
   }
-});
+}
