ed4dfd671767ed28aebd867c3d983613243a4f7f
Seth Silesky <seth.silesky@gmail.com>
2016-11-19 15:19:20 -0600

    add mongoclient, chai http

--- mobile/test/test_actions.js
+++ mobile/test/test_actions.js
@@ -1,15 +1,21 @@
 const chai = require('chai')
+const chaiHttp = require('chai-http');
+const MongoClient = require('mongodb').MongoClient
+chai.use(chaiHttp);
+
+const { expect, request } = chai
   // check if user exists
   // if access token exists, return data -
     // otherwise return error
   // if user doesn't exist, check if user is valid on facebook
   // if facebook says success, then create a new user with the token
-const { expect} = chai;
-const store = require('./../app/stores/store')
+
+import { store }  from './../app/stores/store';
 //const actions = require('./../app/actions/actions');
 module.exports = () => {
     describe('CLIENT: Actions', () => { 
             it ('mocha should work', () => expect(true).to.be.true)
+            console.log(store.getState())
     }   );
     it('when I login successfully, my redux store should populate', () => {
                expect(true).to.be.true;
