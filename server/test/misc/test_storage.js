36d98aea371b81c096ef44ded4ed7d5069f0b295
silesky <seth.silesky@gmail.com>
2016-12-01 18:31:16 -0600

    format test

--- server/test/misc/test_storage.js
+++ server/test/misc/test_storage.js
@@ -6,10 +6,10 @@ const userCollectionJSON = require('./../../seeds/userCollection.json')
 const storage = require('./../../storage')
 
 module.exports = () => {
-    
- describe('STORAGE', function() { // use function declaration so this.timeout works
-  let userCollection;
-  before(() => {
+
+  describe('STORAGE', function () { // use function declaration so this.timeout works
+    let userCollection;
+    before(() => {
       this.timeout(3000);
       MongoClient.connect('mongodb://127.0.0.1:27017/giftr', (err, db) => {
         db.createCollection('userCollection')
@@ -18,30 +18,32 @@ module.exports = () => {
         userCollection().insert(userCollectionJSON);
       })
     })
-      describe('Database Connection -->', () => {
+
+    describe('Database Connection -->', () => {
       it('should connect to gifter db', () => {
         MongoClient.connect('mongodb://127.0.0.1:27017/giftr', (err, db) => {
           expect(db).to.be.ok
         })
       })
-    }) 
-   describe('updateUserByAccessToken,', () => {
- 
-    it('given the old access token, it should update the new one', (done) => {
-      const oldToken = 'f1'
-      const newToken = 'UPDATEDACCESSTOKEN'
-      storage.updateAccessToken('f1', 'zzzz')
-      .then(res => {
-        const expectedMongoResObj = { ok: 1, nModified: 1, n: 1 };
-        //expects { ok: 1, nModified: 1, n: 1 }
-        expect(res).to.be.an.object;
-        expect(res).to.deep.equal(expectedMongoResObj);
-        // done callback makes
-        done();
-      })
-      // if error, don't timeout
-      .catch(done)
     })
-   });
- }); 
+
+    describe('updateUserByAccessToken,', () => {
+      it('given the old access token, it should update the new one', (done) => {
+        const oldToken = 'f1'
+        const newToken = 'UPDATEDACCESSTOKEN'
+        storage.updateAccessToken('f1', 'zzzz')
+          .then(res => {
+            const expectedMongoResObj = { ok: 1, nModified: 1, n: 1 };
+            //expects { ok: 1, nModified: 1, n: 1 }
+            expect(res).to.be.an.object;
+            expect(res).to.deep.equal(expectedMongoResObj);
+            // done callback makes
+            done();
+          })
+          // if error, don't timeout
+          .catch(done)
+      })
+    });
+
+  });
 }
