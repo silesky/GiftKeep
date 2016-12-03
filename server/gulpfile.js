eaefe295d22c821703502775e96b5658a15ab2d3
silesky <seth.silesky@gmail.com>
2016-12-03 00:32:20 -0600

    Gulp files!

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,28 +1,48 @@
 const gulp = require('gulp');
 const nodemon = require('gulp-nodemon');
 const mocha = require('gulp-mocha');
-
+const spawn = require('child_process').spawn,
+node = spawn('node', ['app.js'], {stdio: 'inherit'})
 // make test dependent on nodemon
-
-gulp.task('test', ['nodemon'], () => {
-  return gulp.src('./test/index.js')
+const tester = () => {
+  gulp.src('./test/index.js')
       .pipe(mocha({
         clearRequireCache: true,
         ignoreLeaks: true
-        }));
-});
-gulp.task('nodemon', () => {
+        })
+      .on('error', () => {
+        node.kill()
+        nodemoner()
+      })
+    );
+}
+gulp.task('test', ['nodemon'], tester);
+
+
+const nodemoner = () => {
     nodemon({
-      exec: 'node-inspector & node --debug',
+     // exec: 'node-inspector & node --debug',
       script: 'app.js',
       ext: 'js json',
-      tasks: ['test'],
-      verbose: true,
+      debug: true,
       env: { 'NODE_ENV': 'development' }
   })
-  .on('restart', ['test'])
-});
+    .on('restart', () => {
+      setTimeout(tester, 3000)
+    })
+    .on('crash', () => {
+     console.log('___CRASHED___');
+     node.kill()
+     setTimeout(tester, 15000)
+  })
+  .on('error', () => {
 
+        console.log('___CRASHED___');
+        node.kill()
+         setTimeout(tester, 5000)
+  })
+  }
+gulp.task('nodemon', nodemoner);
 gulp.task('default', ['nodemon']);
 
 
