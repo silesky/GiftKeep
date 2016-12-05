0f8f612c69b8d2f75b4f730239805165a1279004
silesky <seth.silesky@gmail.com>
2016-12-05 13:40:18 -0600

    all gift update / delete works

--- mobile/app/containers/BodyAllGiftsView.js
+++ mobile/app/containers/BodyAllGiftsView.js
@@ -10,33 +10,32 @@ import {
 import * as Utils from './../utils/utils'
 // should get an array of all the gifts
 export class BodyAllGiftsView extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getAllGifts = () => {
-      return Utils.getAllGifts(this.props.state)
-    }
-  }
   static propTypes = {
       isSelected: React.PropTypes.bool,
   }
   render() {
-      
-  const gifts = this.getAllGifts()
+    const gifts = Utils.getAllGifts(this.props.state)
     return (
       <Container>
         <Content>
-           { gifts.map((el, ind) => {
+           { 
+             (gifts.length) 
+                ? 
+                gifts.map((el, ind) => {
+                        const { friendId } = Utils.getFriendByGiftId(this.props.state, el.giftId);
                         return ( 
                             <GiftCard 
-                                // deleteGift={this.props.actions.deleteGift.bind(this, friendId, el.giftId)}
+                                deleteGift={this.props.actions.deleteGift.bind(this, friendId, el.giftId)}
                                 giftDesc={el.giftDesc}
-                                // updateGiftTitle={this.props.actions.updateGiftTitle.bind(this, friendId, el.giftId)}
-                                // updateGiftDesc={this.props.actions.updateGiftDesc.bind(this, friendId, el.giftId)} 
+                                updateGiftTitle={this.props.actions.updateGiftTitle.bind(this, friendId, el.giftId)}
+                                updateGiftDesc={this.props.actions.updateGiftDesc.bind(this, friendId, el.giftId)} 
                                 giftId={el.giftId} 
                                 giftTitle={el.giftTitle} 
                                 key={ind} />
                         )
-                    })} 
+                    }) 
+                    : false
+            } 
           </Content>
       </Container>
     );
