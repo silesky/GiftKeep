9030da02e764c4d52a44b78bdabe5545c073cffe
silesky <seth.silesky@gmail.com>
2016-12-08 16:26:06 -0600

    refactor db

--- server/db.js
+++ server/db.js
@@ -7,6 +7,14 @@ module.exports = {
             this.db = database;
             return database
     }),
+     promiseConnect: () => {
+       return new Promise((resolve, reject) => {
+          MongoClient.connect(process.env.DB_HOST, (err, db) => {
+            if (err) reject(err);
+            if (db) resolve (db)
+          })
+    })
+    },
     connected: () => typeof database !== 'undefined',
     userCollection: () => this.db.collection('userCollection'),
 }
