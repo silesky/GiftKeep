7da9b1f8bf365782b13f7737a61c2ced59287a8a
Seth Silesky <seth.silesky@gmail.com>
2016-12-16 19:23:49 -0600

    try and get nodemon + mocha to play nice (again)

---
+++ server/gulpfile-old.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp');
+const nodemon = require('gulp-nodemon');
+// make test dependent on nodemon
+const tester = () => {
+  gulp.src('./test/index.js')
+      .pipe(mocha({
+        clearRequireCache: true,
+        ignoreLeaks: true
+        })
+      .on('error', () => {
+        node.kill()
+        nodemoner()
+      })
+    );
+}
+gulp.task('test', ['nodemon'], tester);
+
+
+const nodemoner = () => {
+    nodemon({
+     // exec: 'node-inspector & node --debug',
+      script: 'app.js',
+      ext: 'js json',
+      debug: true,
+      env: { 'NODE_ENV': 'development' }
+  })
+    .on('restart', () => {
+      setTimeout(tester, 3000)
+    })
+    .on('crash', () => {
+     console.log('___CRASHED___');
+     node.kill()
+     setTimeout(tester, 15000)
+  })
+  .on('error', () => {
+
+        console.log('___CRASHED___');
+        node.kill()
+         setTimeout(tester, 5000)
+  })
+  }
+gulp.task('nodemon', nodemoner);
+gulp.task('default', ['nodemon']);
+
+
--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,48 +1,37 @@
-const gulp = require('gulp');
-const nodemon = require('gulp-nodemon');
-const mocha = require('gulp-mocha');
-const spawn = require('child_process').spawn,
-node = spawn('node', ['app.js'], {stdio: 'inherit'})
-// make test dependent on nodemon
-const tester = () => {
-  gulp.src('./test/index.js')
-      .pipe(mocha({
-        clearRequireCache: true,
-        ignoreLeaks: true
-        })
-      .on('error', () => {
-        node.kill()
-        nodemoner()
-      })
-    );
-}
-gulp.task('test', ['nodemon'], tester);
+var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var nodemon = require('gulp-nodemon');
 
+const appPath = './app.js';
+gulp.task('nodemon', (cb) => {
+  let started = false;
 
-const nodemoner = () => {
-    nodemon({
-     // exec: 'node-inspector & node --debug',
-      script: 'app.js',
-      ext: 'js json',
-      debug: true,
-      env: { 'NODE_ENV': 'development' }
+  return nodemon({
+    script: appPath
   })
-    .on('restart', () => {
-      setTimeout(tester, 3000)
+    .on('start', () => {
+      if (!started) {
+        started = true;
+        return cb();
+      }
     })
-    .on('crash', () => {
-     console.log('___CRASHED___');
-     node.kill()
-     setTimeout(tester, 15000)
-  })
-  .on('error', () => {
+    .on('restart', () => {
+      console.log('restarting');
+    });
 
-        console.log('___CRASHED___');
-        node.kill()
-         setTimeout(tester, 5000)
-  })
-  }
-gulp.task('nodemon', nodemoner);
-gulp.task('default', ['nodemon']);
+});
 
-
+gulp.task('test', ['nodemon'], function() {
+  return gulp.src('./test/index.js')
+    .pipe(mocha({reporter: 'spec' }))
+    . once('error', function() {
+        process.exit(1);
+    })
+    .once('end', function() {
+      process.exit();
+    });
+});
+gulp.task('watch', () => {
+  gulp.watch(appPath, ['test'])
+});
+gulp.task('default', ['test'])
