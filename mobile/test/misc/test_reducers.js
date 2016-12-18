cd187cbc2e4936f56237b450d105906c710a99c0
silesky <seth.silesky@gmail.com>
2016-12-18 17:19:01 -0600

    format

--- mobile/test/misc/test_reducers.js
+++ mobile/test/misc/test_reducers.js
@@ -1,25 +1,29 @@
 // A reducer should return the new state after applying the action to the previous state.
-import { expect } from 'chai';
+import {
+  expect
+} from 'chai';
 import * as state from './../json/state.json'
 import rootReducer from './../../app/reducers/reducers';
 module.exports = () => {
   describe('REDUCERS:', () => {
-    it('UPDATE_FRIEND should update friend', () => {
-      const existingFriendId = state.user.data[0].friendId,
-        updatedBday = "1-11",
-        updatedFriendName = "MyNewFriendName";
-      let action = {
-        type: 'UPDATE_FRIEND',
-        payload: {
-          friendId: existingFriendId,
-          friendName: updatedFriendName,
-          bday: updatedBday
+    describe('UPDATE_FRIEND', () => {
+      it('should should update friendName', () => {
+        const existingFriendId = state.user.data[0].friendId,
+          updatedBday = "1-11",
+          updatedFriendName = "MyNewFriendName";
+        let action = {
+          type: 'UPDATE_FRIEND',
+          payload: {
+            friendId: existingFriendId,
+            friendName: updatedFriendName,
+            bday: updatedBday
+          }
         }
-      }
-      const currentStateFriendName = rootReducer(state, action).user.data[0].friendName
-      const currentStateBday = rootReducer(state, action).user.data[0].bday
-      expect(currentStateFriendName).to.equal(updatedFriendName);
-      expect(currentStateBday).to.equal(updatedBday)
+        const currentStateFriendName = rootReducer(state, action).user.data[0].friendName
+        const currentStateBday = rootReducer(state, action).user.data[0].bday
+        expect(currentStateFriendName).to.equal(updatedFriendName);
+        expect(currentStateBday).to.equal(updatedBday)
+      })
     })
   });
 }
