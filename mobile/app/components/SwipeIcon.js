66d527c90cb3b2110e87e7107a5ca7c03b02993f
silesky <seth.silesky@gmail.com>
2017-01-21 16:27:25 -0600

    clean up swipeIcons

--- mobile/app/components/SwipeIcon.js
+++ mobile/app/components/SwipeIcon.js
@@ -1,21 +1,14 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
 import { EI as Icon } from './../icons';
 import {View} from 'react-native';
 // https://exponent.github.io/vector-icons/
 export const SwipeIcon = (name, stylesObj) => {
-   const Styles = StyleSheet.create({
-      swipeIcons: {
-        alignSelf: 'center',
-      }
-    })
-   const styles = (stylesObj) ? {...stylesObj, alignSelf: 'center'} : Styles.swipeIcons;
    return [
-     <View key={1} style={{flex:1, justifyContent: 'center'}}>
-            <Icon 
-            name={name}
-            style={styles}
-            />
+     <View key={1} style={{flex: 1, justifyContent: 'center'}}>
+        <Icon 
+          name={name}
+          style={{...stylesObj, alignSelf: 'center'}}
+        />
       </View>
   ]
  }
