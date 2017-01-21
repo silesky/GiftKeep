c256ddf10ebb6a933e0c3a106d0a349b1a7fed3c
silesky <seth.silesky@gmail.com>
2017-01-20 20:02:47 -0600

    allow passing an optional stylesObj to SwipeIcon

--- mobile/app/components/FriendFormEventSwiper.js
+++ mobile/app/components/FriendFormEventSwiper.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { View } from 'react-native';
 import Swipeout from 'react-native-swipeout';
+
+import { SwipeIcon } from '../components/';
 import {
-Card,
+  Card,
   ListItem,
   List,
   CardItem
-
-
 } from 'native-base';
 export const FriendFormEventSwiper = ({ onSwipeDelete, children }) => {
   const swipeoutBtns = [
     {
       type: 'delete',
-      text: 'delete',
+      component: SwipeIcon('trash', {fontSize: 50, marginTop: 45}),
       onPress: () => onSwipeDelete(),
     }
   ]
--- mobile/app/components/SwipeIcon.js
+++ mobile/app/components/SwipeIcon.js
@@ -2,18 +2,19 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { EI as Icon } from './../icons';
 // https://exponent.github.io/vector-icons/
-export const SwipeIcon = (name) => {
+export const SwipeIcon = (name, stylesObj) => {
    const Styles = StyleSheet.create({
       swipeIcons: {
         fontSize: 30,
+        marginTop: 12,
         alignSelf: 'center',
-        marginTop: 12
       }
     })
+   const styles = (stylesObj) ? {...stylesObj, alignSelf: 'center'} : Styles.swipeIcons;
    return [<Icon 
             name={name}
             key={1} 
-            style={Styles.swipeIcons}
+            style={styles}
             />
   ]
  }
