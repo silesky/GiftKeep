2be683e85e6165a4dd4c6c7fef219092941c80a1
silesky <seth.silesky@gmail.com>
2017-01-22 14:58:37 -0600

    refactor index.js to export * from (less verbose)

--- mobile/app/components/index.js
+++ mobile/app/components/index.js
@@ -1,45 +1,15 @@
-import { BottomBar } from './BottomBar';
-export { BottomBar }
-
-import { FbLogin } from './FbLogin';
-export { FbLogin }
-
-
-import { FriendFormDatePicker } from './FriendFormDatePicker';
-export { FriendFormDatePicker }
-
-import { FriendFormEventSwiper } from './FriendFormEventSwiper';
-export { FriendFormEventSwiper }
-
-import { FriendFormEventNameInput } from './FriendFormEventNameInput';
-export { FriendFormEventNameInput }
-
-import { FriendListItem } from './FriendListItem';
-export { FriendListItem }
-
-import { NotificationBottom } from './NotificationBottom';
-export { NotificationBottom }
-
-import { NoFriendsAlert } from './NoFriendsAlert';
-export { NoFriendsAlert }
-
-
-import NoGiftsAlert from './NoGiftsAlert';
-export { NoGiftsAlert } 
-
-import { TopBar } from './TopBar';
-export { TopBar }
-
-import { GiftCard } from './GiftCard';
-export { GiftCard }
-
-import TabBar from './TabBar';
-export { TabBar }
-
-import { SwipeIcon } from './SwipeIcon';
-export { SwipeIcon }
-import { FriendFormEvent } from './FriendFormEvent';
-export { FriendFormEvent }
-
-import { EventCard } from './EventCard';
-export { EventCard }
+export * from './BottomBar';
+export * from './FbLogin';
+export * from './FriendFormDatePicker';
+export * from './FriendFormEventSwiper';
+export * from './FriendFormEventNameInput';
+export * from './FriendListItem';
+export * from './NotificationBottom';
+export * from './NoFriendsAlert';
+export * from './NoGiftsAlert';
+export * from './TopBar';
+export * from './GiftCard';
+export * from './TabBar';
+export * from './SwipeIcon';
+export * from './FriendFormEvent';
+export * from './EventCard';
