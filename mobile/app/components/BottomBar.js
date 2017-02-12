97b3633538ffd8b94b43593789c266bfaebab0b2
silesky <seth.silesky@gmail.com>
2017-02-12 13:44:47 -0600

    bottom bar color

--- mobile/app/components/BottomBar.js
+++ mobile/app/components/BottomBar.js
@@ -1,63 +1,50 @@
-import React from 'react';
-import {
-    Footer,
-    FooterTab,
-    Button,
-    Icon,
-    Text,
-} from 'native-base'
-export const BottomBar = ({
-  addGift,
-  addEvent,
-  friendFormVisibilityToggle,
-  selectedTab,
-}) => {
-  console.log('selectedTab', selectedTab);
+import React from 'react'
+import {Footer, FooterTab, Button, Icon, Text} from 'native-base'
+import colors from '../themes/colors'
+export const BottomBar = ({addGift, addEvent, friendFormVisibilityToggle, selectedTab}) => {
   const whichBtn = () => {
-      let addBtn = {}
-      switch (selectedTab) {
-        case 'gifts':
-          addBtn = {
-            text: 'ADD GIFT',
-            handler: addGift,
-            icon: 'ios-add-circle-outline'
-          }
-          break;
-        case 'events':
-         addBtn = {
-            text: 'ADD EVENT',
-            handler: addEvent,
-            icon: 'md-calendar',
-          }
-          break;
-        case 'all gifts': 
-        addBtn = { 
-           text: 'ADD GIFT',
-           handler: addGift,
-           icon: 'ios-add',
+    let addBtn = {}
+    switch (selectedTab) {
+      case 'gifts':
+        addBtn = {
+          text: 'ADD GIFT',
+          handler: addGift,
+          icon: 'ios-add-circle-outline'
         }
-        break;
-      }
+        break
+      case 'events':
+        addBtn = {
+          text: 'ADD EVENT',
+          handler: addEvent,
+          icon: 'md-calendar'
+        }
+        break
+      case 'all gifts':
+        addBtn = {
+          text: 'ADD GIFT',
+          handler: addGift,
+          icon: 'ios-add'
+        }
+        break
+    }
 
-      return addBtn;
+    return addBtn
   }
 
-    return (
-        <Footer>
-             <FooterTab>
-                   <Button onPress={() => whichBtn().handler()} transparent>
-                      <Text>{whichBtn().text}</Text>
-                    <Icon name={whichBtn().icon} />
-              </Button> 
-          </FooterTab> 
-        </Footer>
-        )
-
+  return (
+    <Footer backgroundColor={colors.$lightgrey2}>
+      <FooterTab>
+        <Button onPress={() => whichBtn().handler()} transparent>
+          <Text>{whichBtn().text}</Text>
+          <Icon name={whichBtn().icon}/>
+        </Button>
+      </FooterTab>
+    </Footer>
+  )
 }
 
 BottomBar.PropTypes = {
   friendFormVisibilityToggle: React.PropTypes.bool,
-  selectedTab: React.PropTypes.oneOf(['gifts', 'events', 'all gifts', ]),
-  addBtnHandler: React.PropTypes.obj,
-
+  selectedTab: React.PropTypes.oneOf(['gifts', 'events', 'all gifts']),
+  addBtnHandler: React.PropTypes.obj
 }
