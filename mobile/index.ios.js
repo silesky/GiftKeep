d0cbe42e766d9a5db5a80ccd0f76bf5b6421a00c
silesky <seth.silesky@gmail.com>
2017-02-18 11:10:51 -0600

    autofix

--- mobile/index.ios.js
+++ mobile/index.ios.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { 
+import React from 'react'
+import { Provider } from 'react-redux'
+import {
     AppRegistry
-} from 'react-native';
+} from 'react-native'
 
-import { store } from './app/stores/stores';
+import { store } from './app/stores/stores'
 
 // needs to be default imports
-import AppContainer from './app/containers/AppContainer';
+import AppContainer from './app/containers/AppContainer'
 
 const Root = () => {
-    return (
+  return (
         <Provider store={store}>
             <AppContainer />
         </Provider>
-    )
+  )
 }
-store.subscribe(Root);
-AppRegistry.registerComponent('mobile', () => Root);
+store.subscribe(Root)
+AppRegistry.registerComponent('mobile', () => Root)
