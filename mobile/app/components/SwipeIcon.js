3f8db3831e53356de70b6bdb20795da06c4558eb
silesky <seth.silesky@gmail.com>
2017-02-18 23:44:25 -0600

    remove accidental fixed width and height tweak for swipeIcon

--- mobile/app/components/SwipeIcon.js
+++ mobile/app/components/SwipeIcon.js
@@ -4,7 +4,7 @@ import {View} from 'react-native';
 // https://exponent.github.io/vector-icons/
 export const SwipeIcon = (name, stylesObj) => {
    return [
-     <View key={1} style={{flex: 1, width: 10, height: 10, justifyContent: 'center'}}>
+     <View key={1} style={{flex: 1, justifyContent: 'center'}}>
         <Icon
           name={name}
           style={{...stylesObj, alignSelf: 'center'}}
