1347faef4769b6d582a09043b72892e70f97c10c
silesky <seth.silesky@gmail.com>
2017-03-11 10:05:41 -0600

    update DrawerContainer so settings cog is hidden https://trello.com/c/ML7fzrGg/154-get-rid-of-settings-toggle-in-top-right

--- mobile/app/containers/DrawerContainer.js
+++ mobile/app/containers/DrawerContainer.js
@@ -1,6 +1,12 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import React, {
+  Component
+} from 'react'
+import {
+  connect
+} from 'react-redux'
+import {
+  bindActionCreators
+} from 'redux'
 import * as actions from './../actions/'
 
 import {
@@ -19,15 +25,19 @@ import {
   List,
   Icon,
   Title
-} from  './../sporks/native-base'
+} from './../sporks/native-base'
 
-import { Header } from './../sporks/native-base'
+import {
+  Header
+} from './../sporks/native-base'
 import {
   FbLogin,
   FriendListItem
 } from './../components/'
 
-import { LightTheme } from './../themes'
+import {
+  LightTheme
+} from './../themes'
 import colors from './../themes/colors'
 class DrawerContainer extends Component {
   constructor (props) {
@@ -37,35 +47,29 @@ class DrawerContainer extends Component {
     return (
       <Container>
         <Header theme={LightTheme} backgroundColor={colors.$headerFooterBg}>
-           <Button
-              onPress={() => this.props.actions.resetAll()}
-              iconRight
-              transparent
-            >
-            <Icon name='ios-settings' />
-        </Button>
-        <Title style={{ color: colors.$bigHeadingTextColor }}>Friends</Title>
-        <Button
-              iconRight
-              onPress={() => this.props.actions.friendFormVisibilityToggle()}
-              transparent
-            >
-          <Icon name='ios-person-add' />
-        </Button>
+          <Button transparent>{``}</Button>
+          <Title style={{
+            color: colors.$bigHeadingTextColor
+          }}>Friends</Title>
+          <Button
+            iconRight
+            onPress={() => this.props.actions.friendFormVisibilityToggle()}
+            transparent>
+            <Icon name='ios-person-add'/>
+          </Button>
         </Header>
         <Content theme={LightTheme}>
           <List>
-            {
-              this.props.state.user.data.map((el, index) => (
+            {this.props.state.user.data.map((el, index) => (
               <FriendListItem
                 onSwipeUpdate={this.props.actions.friendFormUpdateActivate.bind(this, el.friendId)}
                 onSwipeDelete={this.props.actions.deleteFriend.bind(this, el.friendId)}
                 selectFriend={this.props.actions.selectFriend.bind(this, el.friendId)}
                 friendName={el.friendName}
                 key={index}
-                />
+              />
             ))
-            }
+}
           </List>
         </Content>
         <Footer backgroundColor={colors.$headerFooterBg}>
@@ -73,15 +77,18 @@ class DrawerContainer extends Component {
             userName={this.props.state.user.userName}
             fbImage={this.props.state.user.fbImage}
             clear={this.props.actions.clear}
-            authTokenAndTryToGetUser={this.props.actions.authTokenAndTryToGetUser}
-          />
-          </Footer>
+            authTokenAndTryToGetUser={this.props.actions.authTokenAndTryToGetUser}/>
+        </Footer>
       </Container>
 
     )
   }
 }
 
-const mdtp = (dispatch) => ({ actions: bindActionCreators(actions, dispatch) })
-const mstp = (state) => ({ state })
+const mdtp = (dispatch) => ({
+  actions: bindActionCreators(actions, dispatch)
+})
+const mstp = (state) => ({
+  state
+})
 export default connect(mstp, mdtp)(DrawerContainer)
