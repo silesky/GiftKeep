9ade906280841474cac1901f4841c66e209cd67d
silesky <seth.silesky@gmail.com>
2017-04-20 14:17:44 -0500

    android error "unexpected view type nested under text node"

--- mobile/android/app/src/main/java/com/GiftKeeper/MainApplication.java
+++ mobile/android/app/src/main/java/com/GiftKeeper/MainApplication.java
@@ -13,7 +13,16 @@ import com.facebook.react.shell.MainReactPackage;
 import java.util.Arrays;
 import java.util.List;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 public class MainApplication extends Application implements ReactApplication {
+    private static CallbackManager mCallbackManager =
+    CallbackManager.Factory.create();
+
+    protected static CallbackManager getCallbackManager() {
+      return mCallbackManager;
+  }
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
@@ -25,7 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage()
+          new VectorIconsPackage(),
+          new FBSDKPackage(mCallbackManager)
       );
     }
   };
@@ -33,5 +43,11 @@ public class MainApplication extends Application implements ReactApplication {
   @Override
   public ReactNativeHost getReactNativeHost() {
       return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    FacebookSdk.sdkInitialize(getApplicationContext());
   }
 }
