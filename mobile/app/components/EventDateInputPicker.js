d90cbfe860ecb50fc5c09688e9a36f73d60699ab
silesky <seth.silesky@gmail.com>
2017-05-07 12:35:19 -0500

    fix bug where picker hides automatically after month change

--- mobile/app/components/EventDateInputPicker.js
+++ mobile/app/components/EventDateInputPicker.js
@@ -2,23 +2,19 @@ import React, { Component } from 'react'
 import { Input, InputGroup, Icon } from 'native-base'
 import {
   View,
-  Modal,
-  Content,
-  Card,
-  Title,
-  Button,
-  CardItem,
   LayoutAnimation,
   Keyboard,
 } from 'react-native'
 import { EventDatePicker } from './../components/'
 import Moment from 'moment'
 export class EventDateInputPicker extends Component {
+
   static defaultProps = {
     handleOnEventDateChange: null,
     selectedEventDate: new Date(),
     isFocused: false,
   }
+
   constructor (props) {
     super(props)
     this.state = {
@@ -27,18 +23,29 @@ export class EventDateInputPicker extends Component {
     this.onFocus = this.onFocus.bind(this)
     this.onEventDateInputChange = this.onEventDateInputChange.bind(this)
   }
-  onFocus() {
+
+  onFocus () {
     this.togglePicker(true)
     Keyboard.dismiss()
-}
+  }
+
   togglePicker (bool) {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut)
     this.setState({ pickerVisibility: bool })
   }
-  onEventDateInputChange (input) {
-    this.props.handleOnEventDateChange(input)
-    setTimeout(this.togglePicker.bind(this, false), 200)
+
+  onEventDateInputChange (date) {
+    this.props.handleOnEventDateChange(date)
+    const newDate = Moment(date)
+    const oldDate = Moment(this.props.selectedEventDate)
+    const dayHasChanged = newDate.date() !== oldDate.date()
+    const yearHasChanged = newDate.year() !== oldDate.year()
+    // don't hide picker automatically if only month changes
+    if (dayHasChanged || yearHasChanged) {
+      setTimeout(this.togglePicker.bind(this, false), 200)
+    }
   }
+
   render () {
     const {
       selectedEventDate,
@@ -59,7 +66,6 @@ export class EventDateInputPicker extends Component {
             onFocus={() => this.onFocus() }
             autoFocus={isFocused}
             value={Moment(selectedEventDate).format('MM-DD-YYYY')}
-            editable={false}
           />
         </InputGroup>
       </View>
